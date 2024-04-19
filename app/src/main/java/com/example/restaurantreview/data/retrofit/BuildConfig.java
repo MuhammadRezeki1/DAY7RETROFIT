e32887fc892f72ec36a5225d207a45cc3679c90c
MuhammadRezeki1 <mrezeki745@gmail.com>
2024-04-19 19:02:02 +0700

    COMMIT RETRO

---
+++ app/src/main/java/com/example/restaurantreview/data/retrofit/BuildConfig.java
@@ -0,0 +1,12 @@
+package com.example.restaurantreview.data.retrofit;
+
+public class BuildConfig {
+        public static final boolean DEBUG = Boolean.parseBoolean("true");
+        public static final String PACKAGE_NAME = "com.example.myapp";
+        public static final String BUILD_TYPE = "debug";
+        public static final String FLAVOR = "";
+        public static final int VERSION_CODE = 400;
+        public static final String VERSION_NAME = "";
+    }
+
+
